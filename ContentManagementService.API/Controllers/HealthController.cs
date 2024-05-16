6a1d2902caa6efd4c2057f365ad6592948374a47
rastilean <raresastilean@yahoo.com>
2024-05-16 21:20:07 +0300

    Add settings

---
+++ ContentManagementService.API/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContentManagementService.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            try
+            {
+                return Ok("Ahoy! Arrr");
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
